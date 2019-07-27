75eb669f5e2e632b302377416ba75f8b158304e7
James <jmb47580@ucmo.edu>
2019-07-27 14:34:21 -0500

    Added functionality for decimal, zero and clear

--- src/App.js
+++ src/App.js
@@ -19,6 +19,22 @@ class App extends Component {
   addToInput = val => {
     this.setState({ input: this.state.input + val }); //value added
   };
+
+  addZeroToInput = val => {
+    //if this.state.input is not empty add  zero
+    if (this.state.input !== "") {
+      this.setState({ input: this.state.input + val });
+    }
+  };
+  addDecimal = val => {
+    //adds decimal if there is no decimal there
+    if (this.state.indexOf(".") === -1) {
+      this.setState({ input: this.state.input + val });
+    }
+  };
+  clearInput = () => {
+    this.setState({ input: "" });
+  };
   render() {
     return (
       <div className="App">
@@ -28,30 +44,30 @@ class App extends Component {
           </div>
           <div className="row">
             <Button handleClick={this.addToInput}>7</Button>
-            <Button>8</Button>
-            <Button>9</Button>
-            <Button>/</Button>
+            <Button handleClick={this.addToInput}>8</Button>
+            <Button handleClick={this.addToInput}>9</Button>
+            <Button handleClick={this.addToInput}>/</Button>
           </div>
           <div className="row">
-            <Button>4</Button>
-            <Button>5</Button>
-            <Button>6</Button>
-            <Button>*</Button>
+            <Button handleClick={this.addToInput}>4</Button>
+            <Button handleClick={this.addToInput}>5</Button>
+            <Button handleClick={this.addToInput}>6</Button>
+            <Button handleClick={this.addToInput}>*</Button>
           </div>
           <div className="row">
-            <Button>1</Button>
-            <Button>2</Button>
-            <Button>3</Button>
-            <Button>+</Button>
+            <Button handleClick={this.addToInput}>1</Button>
+            <Button handleClick={this.addToInput}>2</Button>
+            <Button handleClick={this.addToInput}>3</Button>
+            <Button handleClick={this.addToInput}>+</Button>
           </div>
           <div className="row">
-            <Button>.</Button>
-            <Button>0</Button>
-            <Button>=</Button>
-            <Button>-</Button>
+            <Button handleClick={this.addToInput}>.</Button>
+            <Button handleClick={this.addToInput}>0</Button>
+            <Button handleClick={this.addToInput}>=</Button>
+            <Button handleClick={this.addToInput}>-</Button>
           </div>
           <div className="row">
-            <ClearButton />
+            <ClearButton handleClear={this.clearInput}>Clear</ClearButton>
           </div>
         </div>
       </div>
